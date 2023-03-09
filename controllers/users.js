42e8adebd34c4970f7d29e84a47ce32b07a40ec6
Evening <evening@mstars-lab3-07s-iMac.local>
2023-03-09 18:04:28 +0800

    jj

--- controllers/users.js
+++ controllers/users.js
@@ -1,67 +1,69 @@
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
-const filePath="./data/users.json.";
-const getAllUsers= (req, res) => {
-    fs.readFile(filePath, "utf-8", (err, data) => {
+const filePath = "./data/users.json.";
+const connection = require("../config/db");
+
+const getAllUsers = (req, res) => {
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      console.log("Файл уншихад алдаа гарлаа. !!!");
+      return;
+    }
+    console.log(data);
+    const parsedData = JSON.parse(data);
+    res.status(201).json({ users: parsedData.users });
+  });
+};
+
+const getUser = (req, res) => {
+  const { id } = req.params;
+  const data = fs.readFileSync(filePath, "utf-8");
+  const parsedData = JSON.parse(data);
+  const user = parsedData.users.find((el) => el.id === id);
+  res.status(200).json({ user });
+};
+const createUser = (req, res) => {
+  const { name, email, password, phoneNumber } = req.body;
+  const salted = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salted);
+
+  const query =
+    "INSERT INTO users (id, role, name,email,password, phone_number, profileImg) VALUES(null, null, ?, ?, ?, ?, ?)";
+  connection.query(
+    query,
+    [name, email, hashedPassword, phoneNumber, "url"],
+    (err, result) => {
       if (err) {
-        console.log("Файл уншихад алдаа гарлаа. !!!");
+        res.status(400).json({ error: err.message });
         return;
       }
-      console.log(data);
-      const parsedData = JSON.parse(data);
-      res.status(201).json({ users: parsedData.users });
-    });
-  };
-  
-  const getUser= (req, res) => {
-    const { id } = req.params;
-    const data = fs.readFileSync(filePath, "utf-8");
-    const parsedData = JSON.parse(data);
-    const user = parsedData.users.find((el) => el.id === id);
-    res.status(200).json({ user });
-  };
-  const createUser=(req, res) => {
-    const { name, role = "user", email, password } = req.body;
-    const data = fs.readFileSync(filePath, "utf-8");
-    const parsedData = JSON.parse(data);
-    const id = uuidv4();
-    const salted = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(password, salted);
-    console.log(hashedPassword);
-    const newUser = {
-      id,
-      name,
-      role,
-      email,
-      password: hashedPassword,
-    };
-    parsedData.users.push(newUser);
-    fs.writeFileSync(filePath, JSON.stringify(parsedData));
-    res.status(201).json({ message: "Шинэ хэрэглэгчийгн амжилттай бүртгэлээ." });
-  };
-  const deleteUser =(req, res) => {
-    const { id } = req.params;
-    const data = fs.readFileSync(filePath, "utf-8");
-    const parsedData = JSON.parse(data);
-    const findIndex = parsedData.users.findIndex((el) => el.id === id);
-    parsedData.users.splice(findIndex, 1);
-    fs.writeFileSync(filePath, JSON.stringify(parsedData));
-    res
-      .status(201)
-      .json({ message: `${id} тай хэрэглэгч амжилттай устгагдлаа.` });
-  };
-  const updateUser=(req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
-    const data = fs.readFileSync(filePath, "utf-8");
-    const parsedData = JSON.parse(data);
-    const findIndex = parsedData.users.findIndex((el) => el.id === id);
-    parsedData.users[findIndex].name = name;
-    fs.writeFileSync(filePath, JSON.stringify(parsedData));
-    res
-      .status(201)
-      .json({ message: "Шинэ хэрэглэгчийн өгөгдөл амжилттай солигдлоо." });
-  };
-  module.exports={getAllUsers ,getUser, createUser , deleteUser,updateUser};
-  
+      res
+        .status(201)
+        .json({ message: "Шинэ хэрэглэгч амжилттай бүртгэгдлээ." });
+    }
+  );
+};
+const deleteUser = (req, res) => {
+  const { id } = req.params;
+  const data = fs.readFileSync(filePath, "utf-8");
+  const parsedData = JSON.parse(data);
+  const findIndex = parsedData.users.findIndex((el) => el.id === id);
+  parsedData.users.splice(findIndex, 1);
+  fs.writeFileSync(filePath, JSON.stringify(parsedData));
+  res
+    .status(201)
+    .json({ message: `${id} тай хэрэглэгч амжилттай устгагдлаа.` });
+};
+const updateUser = (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const data = fs.readFileSync(filePath, "utf-8");
+  const parsedData = JSON.parse(data);
+  const findIndex = parsedData.users.findIndex((el) => el.id === id);
+  parsedData.users[findIndex].name = name;
+  fs.writeFileSync(filePath, JSON.stringify(parsedData));
+  res
+    .status(201)
+    .json({ message: "Шинэ хэрэглэгчийн өгөгдөл амжилттай солигдлоо." });
+};
+module.exports = { getAllUsers, getUser, createUser, deleteUser, updateUser };
--- server.js
+++ server.js
@@ -3,99 +3,16 @@ const cors = require("cors");
 const usersRoute = require("./routes/users");
 const categoriesRoute = require("./routes/categories");
 const logger = require("./utils/logger");
-const port = 8001;
+const port = 8000;
 const server = express();
-const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "azure_db",
-});
 server.use(cors());
 server.use(express.json());
 
 server.use(logger);
 
-server.get("/", async (req, res) => {
-  connection.query("SELECT * FROM azure_user", (err, result) => {
-    if (err) {
-      res.status(400).json({ message: err.message });
-
-      return;
-    }
-    res.status(200).json({ message: "success", data: result });
-  });
-});
-
-server.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  connection.query(
-    `SELECT * FROM azure_user WHERE aid=${id}`,
-    (err, result) => {
-      if (err) {
-        res.status(400).json({ message: err.message });
-
-        return;
-      }
-      res.status(200).json({ message: "success", data: result });
-    }
-  );
-});
-
-server.post("/", async (req, res) => {
-  const { id } = req.params;
-  connection.query(
-    `INSERT INTO azure_user(aid, name ,owog) VALUE(${id},"${req.body.name}","${req.body.owog}")`,
-    (err, result) => {
-      if (err) {
-        res.status(400).json({ message: err.message });
-
-        return;
-      }
-      res.status(200).json({ message: "success", data: result });
-    }
-  );
-});
-
-server.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const keys = Object.keys(body);
-  const map = keys.map((key) => {
-    `${key}="${body[key]}"`;
-  });
-  const join = map.join();
-
-  connection.query(
-    `UPDATE azure_user SET ${join} WHERE aid=${id}`,
-    (err, result) => {
-      if (err) {
-        res.status(400).json({ message: err.message });
-
-        return;
-      }
-      res.status(200).json({ message: "success", data: result });
-    }
-  );
-});
-
-server.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  connection.query(`DELETE FROM azure_user WHERE aid=${id}`, (err, result) => {
-    if (err) {
-      res.status(400).json({ message: err.message });
-
-      return;
-    }
-    res.status(200).json({ message: "success", data: result });
-  });
-});
-
-server.use("/users", usersRoute);
-server.use("/categories", categoriesRoute);
+server.use("/api/users", usersRoute);
+server.use("/api/categories", categoriesRoute);
 
 server.listen(port, () => {
   console.log(`Server is running at ${port}`);
